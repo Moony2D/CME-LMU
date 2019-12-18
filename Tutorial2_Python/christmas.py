7e6bb099c08ba0239762c49e9491c0137ae971e1
Alex Schmitt <schmitt@ifo.de>
2019-12-18 11:51:05 +0100

    Addition to christmas.py

--- Tutorial2_Python/christmas.py
+++ Tutorial2_Python/christmas.py
@@ -3,6 +3,7 @@ from shapes import *
 from random import randint
 
 import webbrowser
+import IPython.display as display
 
 class Santa():
     """ 
@@ -91,6 +92,42 @@ class Christkind(Santa):
         self.num_of_gifts = num_of_gifts
         self.song = favorite_song
         
+    def draw_tree(self):
+        """
+        Method that draws a christmas tree
+        """
+        myPen = Turtle()
+        myPen.shape("turtle")
+        myPen.speed(25)
+
+        window = turtle.Screen()
+        window.bgcolor("#69D9FF")
+
+        y = -100
+        width = 240
+
+        #let's draw the trunk of the tree
+        draw_rectangle(myPen, "brown", -15, y-40, 30, 40)
+
+        #Now the tree itself
+        while width>20:
+            width = width - randint(20,30)
+            height = randint(15,35)
+            x = 0 - width/2
+            draw_rectangle(myPen, "green", x, y, width, height)
+            y = y + height
+
+        #And finally let's add a star at the top of our tree.
+        draw_star(myPen, "yellow", 4, y, 20)
+
+        myPen.penup()
+        myPen.color("red")
+        myPen.goto(-20, -180)
+        myPen.write("Merry Christmas and a Happy New Year!", align = 'center', font = ('Source Sans Pro', 18, 'bold'))
+        myPen.hideturtle() 
+        done()
+
+
     
     
     
